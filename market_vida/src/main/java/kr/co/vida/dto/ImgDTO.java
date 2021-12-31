a96f344b01e7f042f95a9b127ffb4342e9afba06
sohyun <dogcow909@naver.com>
2021-12-31 17:37:57 +0900

    img commit

---
+++ market_vida/src/main/java/kr/co/vida/dto/ImgDTO.java
@@ -0,0 +1,18 @@
+package kr.co.vida.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImgDTO {
+	
+	private int imgNo;
+	private int goodsNo;
+	private String imgName;
+	private int imgStatus;
+	
+
+}
