3c36b38f30f03aa8a7e5c7b67a252ebb3da11524
MJ <rlaalswns0147@naver.com>
2021-12-31 17:48:10 +0900

    5:31

--- market_vida/src/main/java/kr/co/vida/dao/Dao.java
+++ market_vida/src/main/java/kr/co/vida/dao/Dao.java
@@ -1,5 +1,18 @@
 package kr.co.vida.dao;
 
-public interface Dao {
+import java.util.List;
 
+import kr.co.vida.dto.CouponBoxDTO;
+import kr.co.vida.dto.CouponDTO;
+
+public interface Dao {
+	public List<CouponDTO> getCouponList();
+	public CouponDTO getCouponOne(int no);
+	public void addCouponOne(CouponDTO cdto);
+	public void deleteCoupon(int no);
+	
+	public List<CouponBoxDTO> getCouponBoxList();
+	public CouponBoxDTO getCouponBoxOne(int no);
+	public void addCouponBoxOne(CouponBoxDTO cbdto);
+	public void modifyCouponBox(CouponBoxDTO cbdto);
 }
