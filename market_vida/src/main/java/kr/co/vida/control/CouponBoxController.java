5f076462611ddc96dd20219d0f02d488acc6299e
MJ <rlaalswns0147@naver.com>
2022-01-03 14:29:55 +0900

    2:29

--- market_vida/src/main/java/kr/co/vida/control/CouponBoxController.java
+++ market_vida/src/main/java/kr/co/vida/control/CouponBoxController.java
@@ -4,10 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
+import kr.co.vida.dto.CouponBoxDTO;
 import kr.co.vida.service.VidaService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,16 +16,15 @@ public class CouponBoxController {
 
 	@Autowired
 	@Qualifier("CouponBoxService")
-	VidaService service;
+	VidaService<CouponBoxDTO> service;
 
-	public void setService(VidaService service) {
+	public void setService(VidaService<CouponBoxDTO> service) {
 		this.service = service;
 	}
 
 	@RequestMapping("myCoupon")
 	public String myCoupon(Model model) {
-		model.addAttribute("list",service.getCouponBoxList());
-		System.out.println();
+		model.addAttribute("list",service.selectAllList());
 		return "mypage/myCoupon";
 	}
 
--- market_vida/src/main/java/kr/co/vida/dao/ImgDAO.java
+++ market_vida/src/main/java/kr/co/vida/dao/ImgDAO.java
@@ -4,12 +4,8 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-import kr.co.vida.dto.CouponBoxDTO;
-import kr.co.vida.dto.CouponDTO;
 import kr.co.vida.dto.ImgDTO;
 
 @Repository
