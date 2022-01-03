b395a9266ec04f38ba14f650f70863432bfb20e5
MJ <rlaalswns0147@naver.com>
2022-01-03 14:03:03 +0900

    2:02

--- market_vida/src/main/java/kr/co/vida/control/CouponBoxController.java
+++ market_vida/src/main/java/kr/co/vida/control/CouponBoxController.java
@@ -3,14 +3,17 @@ package kr.co.vida.control;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import kr.co.vida.service.VidaService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
-public class CouponController {
+public class CouponBoxController {
 
 	@Autowired
 	@Qualifier("CouponBoxService")
@@ -20,9 +23,11 @@ public class CouponController {
 		this.service = service;
 	}
 
-	@RequestMapping("/myCoupon")
-	public String myCoupon() {
-		return "myCoupon";
+	@RequestMapping("myCoupon")
+	public String myCoupon(Model model) {
+		model.addAttribute("list",service.getCouponBoxList());
+		System.out.println();
+		return "mypage/myCoupon";
 	}
 
 }
--- market_vida/src/main/webapp/views/mypage/myCoupon.jsp
+++ market_vida/src/main/webapp/views/mypage/myCoupon.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +9,15 @@
 </head>
 <body>
 	<h1>myCoupon.jsp</h1>
+	
+	<table>
+		<c:forEach var="dto" items="${list }">
+		<tr>
+			<th>쿠폰번호</th>
+			<td>${dto.coupon_code }</td>
+		</tr>
+		</c:forEach>
+	</table>
+	<h2>${list}</h2>
 </body>
 </html>
