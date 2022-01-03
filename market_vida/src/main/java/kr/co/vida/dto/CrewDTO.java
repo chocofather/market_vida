4e1a0a5daccac2d47f7c14226d761a5024b28a22
JD <jd0922@naver.com>
2022-01-03 10:03:48 +0900

    commit

---
+++ market_vida/src/main/java/kr/co/vida/dto/CrewDTO.java
@@ -0,0 +1,12 @@
+package kr.co.vida.dto;
+
+public class MemberDTO {
+	private String Mem_no;
+	private String Id;
+	private String Name;
+	private String Pwd;
+	private String Addrs;
+	private String Email;
+	
+	
+}
