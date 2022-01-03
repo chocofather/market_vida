a2f0411444730fc84c7e889ea85166f0b2bdeae9
JD <jd0922@naver.com>
2022-01-03 17:11:14 +0900

    commit

--- market_vida/src/main/java/kr/co/vida/control/MainController.java
+++ market_vida/src/main/java/kr/co/vida/control/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	@RequestMapping("main.do")
+	@RequestMapping(value= {"/","/main"})
 	public String main() {
 		return "main";
 	}
