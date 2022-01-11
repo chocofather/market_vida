3c2637159775be727e8239f8f8f23a669e22dd08
LDY <jmis847@naver.com>
2022-01-11 15:15:19 +0900

    commit

--- market_vida/src/main/java/kr/co/vida/dao/Dao.java
+++ market_vida/src/main/java/kr/co/vida/dao/Dao.java
@@ -3,10 +3,6 @@ package kr.co.vida.dao;
 import java.util.List;
 
 public interface Dao<T> {
-<<<<<<< HEAD
-//	public List<T> getListAll(int no);
-=======
->>>>>>> refs/heads/main
 	public List<T> getListAll();
 	public T getOne(int no);
 	public void addOne(T dto);
