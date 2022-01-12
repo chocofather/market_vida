60d62e10a355fd25205d6b0df9ae7292489a9ef0
LDY <jmis847@naver.com>
2022-01-12 10:46:07 +0900

    create wirteQnaBoard jsp

--- market_vida/src/main/java/kr/co/vida/control/QnaBoardController.java
+++ market_vida/src/main/java/kr/co/vida/control/QnaBoardController.java
@@ -3,11 +3,13 @@ package kr.co.vida.control;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.vida.service.QnaBoardImple;
 
 @Controller
+@RequestMapping("/board")
 public class QnaBoardController {
 	@Autowired
 	QnaBoardImple service;
@@ -16,5 +18,10 @@ public class QnaBoardController {
 	public String qna(Model model) {
 		model.addAttribute("list",service.selectAllList());
 		return "board/qnaBoard";
+	}
+	
+	@GetMapping("/writeQna")
+	public String writeQna() {
+		return "board/writeQnaForm";
 	}
 }
--- market_vida/src/main/webapp/views/board/qnaBoard.jsp
+++ market_vida/src/main/webapp/views/board/qnaBoard.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,6 +21,11 @@
 				<td>${dto.qna_date }</td>
 			</tr>
 		</c:forEach>
+		<tr>
+			<td colspan="3">
+				<a href="./writeQna">1:1문의 등록</a>
+			</td>
+		</tr>
 	</table>
 </body>
 </html>
