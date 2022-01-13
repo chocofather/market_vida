c02435b0ebe63902528eebf0afc50692d85c01c1
sohyun <dogcow909@naver.com>
2022-01-13 15:55:24 +0900

    update commit

--- market_vida/src/main/java/kr/co/vida/control/GoodsController.java
+++ market_vida/src/main/java/kr/co/vida/control/GoodsController.java
@@ -11,13 +11,8 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-=======
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
->>>>>>> branch 'main' of https://github.com/chocofather/market_vida.git
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -31,6 +26,7 @@ import oracle.jdbc.proxy.annotation.Post;
 
 @Slf4j
 @Controller
+@RequestMapping("goods")
 public class GoodsController {
 	
 	@Autowired
@@ -41,29 +37,15 @@ public class GoodsController {
 	
 	@Autowired
 	GoodsImple goodsService;
-
-
-	@GetMapping("/goods/goodsList")
-	public String test(Model model) {
-	//	public String test(Model model, @RequestParam("main_cat_code")int main_cat_code) {
-	//	ï¿½ï¿½ï¿½Î°ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½
-		
-		
-		return null;
-	}
 	
 
 	@GetMapping("/goodsList")
 	public String goodsList(@RequestParam("cat_code")int cat_code, Model model) {
 	//	public String goodsList(Model model, @RequestParam("main_cat_code")int main_cat_code) {
-<<<<<<< HEAD
-	//	¸ŞÀÎ°ú °áÇÕ ÈÄ »ç¿ë
+	//	ë©”ì¸ê³¼ ê²°í•© í›„ ì‚¬ìš©
 		log.info("subCode=====>"+cat_code);
-=======
-	//	ï¿½ï¿½ï¿½Î°ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½
->>>>>>> branch 'main' of https://github.com/chocofather/market_vida.git
 		
-		// ¸ŞÀÎ ÄÚµå 1°³¸¸ ±¸ÇÏ±â
+		// ë©”ì¸ ì½”ë“œ 1ê°œë§Œ êµ¬í•˜ê¸°
 		int forMainCode;	
 		if(cat_code%100==0) {
 			forMainCode = cat_code+1;
@@ -71,12 +53,12 @@ public class GoodsController {
 			forMainCode = cat_code;
 		}
 		
-		// Ä«Å×°í¸® ¸®½ºÆ® °´Ã¼
+		// ì¹´í…Œê³ ë¦¬ ë¦¬ìŠ¤íŠ¸ ê°ì²´
 		SubCatDTO subDto = subService.selectOne(forMainCode);
 		model.addAttribute("mainCode", subDto);
 		model.addAttribute("subDtoList", subService.getListAll(subDto.getMain_cat_code()));
 		
-		// »óÇ° ¸®½ºÆ® °´Ã¼
+		// ìƒí’ˆ ë¦¬ìŠ¤íŠ¸ ê°ì²´
 		if (cat_code %100==0) {
 			model.addAttribute("imgDto", imgService.selectAllList(cat_code));
 		}else {
@@ -86,43 +68,18 @@ public class GoodsController {
 		return "/goods/goodsList";
 	}
 	
-<<<<<<< HEAD
 	@RequestMapping("/deleteGoods")
-=======
-	@PostMapping("/goods/goodsListAjax")
-	public @ResponseBody Map<String, Object> subCatGoodsList(@RequestParam("subCode")int subCode, Model model) {
-//		model.addAttribute("imgBySubCod", imgDao.getListBySubCode(subCode));
-		HashMap<String, Object> dataMap = new HashMap<String, Object>();
-		HashMap<String, Object> data = new HashMap<String, Object>();
-//		mav.addObject("imgBySubCod", imgDao.getListBySubCode(subCode));
-		dataMap.put("imgBySubCod", data);
-		return dataMap;
-	}
-	
-	@GetMapping(value = "/goodsListAjax", produces = MediaType.APPLICATION_JSON_VALUE)
->>>>>>> branch 'main' of https://github.com/chocofather/market_vida.git
 	@ResponseBody
-<<<<<<< HEAD
 	public String deleteGoods(HttpServletRequest req){
 		String []chkBoxList = req.getParameterValues("deletecb");
 		for (String goodsNo : chkBoxList) {
 			System.out.println(goodsNo);
 		}
 		return "delete success";
-=======
-	public Map<String, Object> subCatGoodsList(@RequestParam("subCode")int subCode) {
-
-		log.info("subCode=====>"+subCode);
-		
-		HashMap<String, Object> dataMap = new HashMap<String, Object>();
-		dataMap.put("imgBySubCode", imgService.getListBySubCode(subCode));
-		
-		return dataMap;
->>>>>>> branch 'main' of https://github.com/chocofather/market_vida.git
 	}
 	
 	
-	// »óÇ° µğÅ×ÀÏ ÆäÀÌÁö ÀÌµ¿
+	// ìƒí’ˆ ë””í…Œì¼ í˜ì´ì§€ ì´ë™
 	@GetMapping("/goodsDetail")
 	public String goodsDetail(@RequestParam("goods_no")int goods_no, Model model) {
 		
@@ -135,15 +92,5 @@ public class GoodsController {
 	}
 	
 	
-	@GetMapping("/admin/goodswrite")
-	public String writeForm() {
-		return "/admin/productRegister";
-	}
-
-	@PostMapping("/admin/goodswrite")
-	public String writeFormOk(@ModelAttribute("dto") GoodsDTO dto, HttpServletRequest req) {
-		goodsService.insertOne(dto);
-		return "redirect:/main";
-	}
 
 }
